179693c3e17401d28414f1395b00d96c498e834c
松田　遥 <hayutoverycute@gmail.com>
2025-02-18 22:00:46 +0900

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,34 @@
+document.write("<p><b><big>課題</big></b></p>")
+
+document.write("<p>1.xがyより大きい時にifの処理へ入るようにしてください。</p>")
+
+var x = 20
+var y = 10
+if (x > y) {
+console.log("xはyより大きい");
+}
+
+document.write("<p>2.xがyより小さい時にifの処理へ入るようにしてください。</p>")
+
+var x = 10
+var y = 20
+if (x < y) {
+console.log("xはyより小さい");
+}
+
+document.write("<p>3.xがy以上の時にifの処理へ入るようにしてください。</p>")
+
+var x = 20
+var y = 10
+if (x >= y) {
+console.log("xがy以上");
+}
+
+document.write("<p>4.xがy以下の時にifの処理へ入るようにしてください。</p>")
+
+var x = 10
+var y = 20
+if (x <= y) {
+console.log("xがy以下");
+}
+
