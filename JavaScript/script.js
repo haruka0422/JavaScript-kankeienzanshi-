af700ecb9163645a385a3d084eab34248536b9a9
松田　遥 <hayutoverycute@gmail.com>
2025-02-25 21:17:16 +0900

    2nd commit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,34 +1,33 @@
-document.write("<p><b><big>課題</big></b></p>")
+document.write("<p><b><big>課題</big></b></p>");
 
-document.write("<p>1.xがyより大きい時にifの処理へ入るようにしてください。</p>")
+document.write("<p>1.xがyより大きい時にifの処理へ入るようにしてください。</p>");
 
-var x = 20
-var y = 10
-if (x > y) {
-console.log("xはyより大きい");
+let x1 = 20;
+let y1 = 10;
+if (x1 > y1) {
+  console.log("xはyより大きい");
 }
 
-document.write("<p>2.xがyより小さい時にifの処理へ入るようにしてください。</p>")
+document.write("<p>2.xがyより小さい時にifの処理へ入るようにしてください。</p>");
 
-var x = 10
-var y = 20
-if (x < y) {
-console.log("xはyより小さい");
+let x2 = 10;
+let y2 = 20;
+if (x2 < y2) {
+  console.log("xはyより小さい");
 }
 
-document.write("<p>3.xがy以上の時にifの処理へ入るようにしてください。</p>")
+document.write("<p>3.xがy以上の時にifの処理へ入るようにしてください。</p>");
 
-var x = 20
-var y = 10
-if (x >= y) {
-console.log("xがy以上");
+let x3 = 20;
+let y3 = 10;
+if (x3 >= y3) {
+  console.log("xがy以上");
 }
 
-document.write("<p>4.xがy以下の時にifの処理へ入るようにしてください。</p>")
+document.write("<p>4.xがy以下の時にifの処理へ入るようにしてください。</p>");
 
-var x = 10
-var y = 20
-if (x <= y) {
-console.log("xがy以下");
+let x4 = 10;
+let y4 = 20;
+if (x4 <= y4) {
+  console.log("xがy以下");
 }
-
